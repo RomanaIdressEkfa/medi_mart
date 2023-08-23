40db55c1e25db6e9773874727fdd7e199b21df77
suraya2509xyz <surayaislampaiker@gmail.com>
2023-08-23 15:59:01 +0600

    complete medicine crud

--- resources/views/backend/layouts/medicine_details/create.blade.php
+++ resources/views/backend/layouts/medicine_details/create.blade.php
@@ -7,14 +7,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vendor Details Form</title>
+  <title>Medicine Form</title>
   <!-- Include Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
 
   <div class="container mt-5">
-    <h2>Vendor Details Form</h2>
+    <h2>Medicine Form</h2>
 
 
     @if ($errors->any())
--- resources/views/backend/layouts/medicine_details/index.blade.php
+++ resources/views/backend/layouts/medicine_details/index.blade.php
@@ -7,13 +7,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vendor Details</title>
+  <title>Medicine List</title>
   <!-- Include Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
   <div class="container mt-5">
-    <h2>Vendor Details</h2>
+    <h2>Medicine List</h2>
     <table class="table">
       <thead>
         <tr>
